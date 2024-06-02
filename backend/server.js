70d427677dbeb1a4c4f7a11aa084b4be9c3eb473
supremex04 <bhandarisupreme7@gmail.com>
2024-06-02 20:26:22 +0545

    setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
 const express = require("express")
+const dotenv = require("dotenv");
+const app = express();
+dotenv.config();
+PORT = process.env.PORT || 5000
+app.get("/", (req,res) => {
+    // root route http://localhost:5000
+    res.send("Hello world.");
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
