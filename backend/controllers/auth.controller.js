9e30e5b42508daf333e483284acf32f3530b1951
supremex04 <bhandarisupreme7@gmail.com>
2024-06-04 20:41:38 +0545

    controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,22 @@
+import User from "../models/user.model.js";
+
 export const signup = async (req, res) => {
     try{
         const {fullName, username, password, confirmPassword, gender} = res.body;
+        if (password !== confirmPassword){
+            return res.status(400).json({error: "Passwords dont match!"});
+        }
+
+        const user = await User.findOne({username});
+
+        if (user){
+            return res.status(400).json({error:"Username already exists"});
+        }
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+        // hash password
     } catch (error){
 
     }
--- backend/server.js
+++ backend/server.js
@@ -7,16 +7,16 @@ dotenv.config();
 const PORT = process.env.PORT || 5000
 // The get method is used to respond to HTTP GET requests, which are 
 // typically used to request data from the server.
-app.get("/", (req,res) => {
-    //  When you access http://localhost:5000/ in your browser, you are making 
-    // a GET request to the root path. ie "/"
-    res.send("Hello world.");
-    //  The res.send method sends the specified string ("Hello world.") 
-    // as the response body. When a client (like a web browser) makes a GET request 
-    // to the root URL, it will receive this string in response and display it.
+// app.get("/", (req,res) => {
+//     //  When you access http://localhost:5000/ in your browser, you are making 
+//     // a GET request to the root path. ie "/"
+//     res.send("Hello world.");
+//     //  The res.send method sends the specified string ("Hello world.") 
+//     // as the response body. When a client (like a web browser) makes a GET request 
+//     // to the root URL, it will receive this string in response and display it.
 
-});
-
+// });
+app.use(express.json());  // to parse the incoming requests with JSON payloads (from req.body)
 app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
     connectToMongoDB();
