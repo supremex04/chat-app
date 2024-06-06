cc19d6f6a4e5368f0fec3d6122eb482c47ce9420
supremex04 <bhandarisupreme7@gmail.com>
2024-06-06 19:46:43 +0545

    hash pass

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,8 @@
+import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
-
 export const signup = async (req, res) => {
     try{
+        // taking input from user
         const {fullName, username, password, confirmPassword, gender} = req.body;
         if (password !== confirmPassword){
             return res.status(400).json({error: "Passwords dont match!"});
@@ -13,6 +14,8 @@ export const signup = async (req, res) => {
             return res.status(400).json({error:"Username already exists"});
         }
         // hash password here
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
         const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
         const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
@@ -20,17 +23,21 @@ export const signup = async (req, res) => {
         const newUser = new  User({
             fullName,
             username,
-            password,
+            password:hashedPassword,
             gender,
             profilePic: gender === "male" ? boyProfilePic : girlProfilePic
         })
-        await newUser.save();
-        res.status(201).json({
-            _id: newUser._id,
-            fullName: newUser.fullName,
-            username: newUser.username,
-            profilePic: newUser.profilePic
-        })
+        if (newUser){
+            await newUser.save();
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                profilePic: newUser.profilePic
+            });}
+        else{
+            res.status(400).json( {error: "Invalid user data"});
+        }
         
     } catch (error){
         console.log("Error in signup controller", error.message);
